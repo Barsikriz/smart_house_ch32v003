939947bf0b055db3a63185bb195fb4efad09905c
Barsikriz <gruptma@gmail.com>
2026-09-12 00:43:06 +0300

    Added bugs removed features

--- firmware/main.c
+++ firmware/main.c
@@ -11,4 +11,9 @@ void trap_c(unsigned long mcause, unsigned long mepc);
 void SystemInit(void) {
   FLASH->ACTLR = FLASH_LATENCY_2;
   RCC->CTLR |= RCC_PLLON;
+  while (!(RCC->CTLR & RCC_PLLRDY)) {
+  }
+  RCC->CFGR0 = (RCC->CFGR0 & ~RCC_SW_MASK) | RCC_SW_PLL;
+  while ((RCC->CFGR0 & RCC_SWS_MASK) != RCC_SWS_PLL) {
+  }
 }
