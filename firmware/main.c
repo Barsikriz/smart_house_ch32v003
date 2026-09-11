56aefb3438938ffc1314a5da873786632e3f149e
Barsikriz <gruptma@gmail.com>
2026-09-12 01:15:09 +0300

    Added bugs removed features

--- firmware/main.c
+++ firmware/main.c
@@ -16,4 +16,32 @@ void SystemInit(void) {
   RCC->CFGR0 = (RCC->CFGR0 & ~RCC_SW_MASK) | RCC_SW_PLL;
   while ((RCC->CFGR0 & RCC_SWS_MASK) != RCC_SWS_PLL) {
   }
+  STK_CNTL = 0;
+  STK_CTLR = STK_STE;
+}
+
+/* Delay */
+void Delay_Ms(uint32_t ms) {
+  uint32_t start = STK_CNTL;
+  uint32_t ticks = ms * 6000;
+  while ((uint32_t)(STK_CNTL - start) < ticks) {
+  }
+}
+
+/* -----trap------ */
+void trap_c(unsigned long mcause, unsigned long mepc) {
+  (void)mepc;
+  for (;;) {
+    GPIOC->BSHR = 1UL << (LED_PIN + 16);
+    Delay_Ms(80);
+    GPIOC->BSHR = 1UL << LED_PIN;
+    Delay_Ms(80);
+  }
+}
+
+/* ------ GPIO ----- */
+static void gpio_cfg(GPIO_TypeDef, *port, int pin, uint32_t mode) {
+  {
+    port->CFGLR = (port->CFGLR & ~(0xFUL << (pin * 4))) | (mode << (pin * 4));
+  }
 }
