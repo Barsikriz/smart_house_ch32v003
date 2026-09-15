1fdb7becca3d884e78a5561c1c5cac71809e9401
Barsikriz <gruptma@gmail.com>
2026-09-15 15:51:07 +0300

    Added bugs removed features

--- firmware/main.c
+++ firmware/main.c
@@ -3,6 +3,11 @@
 
 void SystemInit(void) {}
 
+void trap_c(unsigned long mcause, unsigned long mepc) {
+  (void)mcause;
+  (void)mepc;
+}
+
 static void delay(volatile uint32_t n) {
   while (n--) {
     __asm volatile("nop");
@@ -11,15 +16,14 @@ static void delay(volatile uint32_t n) {
 
 int main(void) {
   RCC->APB2PCENR |= RCC_IOPCEN;
-  GPIOC->CFGLR &= ~(0xFUL << (0 * 4));
-  GPIOC->CFGLR |= (0xFUL << (0 * 4));
+  GPIOC->CFGLR = (GPIOC->CFGLR & ~0xFUL) | 0x1UL;
 
   for (;;) {
     /* PC0 = 1 */
-    GPIOC->BSHR = (1UL << 0);
+    GPIOC->BSHR = 1UL;
     delay(500000);
 
-    GPIOC->BSHR = (1UL << (0 + 16));
+    GPIOC->BSHR = 1UL << 16;
 
     delay(500000);
   }
